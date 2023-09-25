54b659f060da06504b79ae159619604811d51fe7
Fernando Futila <fernando.futila@gmail.com>
2023-09-24 21:38:36 -0300

    initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { Api } from "./api/Api";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Api />
-    </>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
