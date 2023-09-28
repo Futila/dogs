91f90ebdaf822ee973fb5014a83d039f73da1e9b
Fernando Futila <fernando.futila@gmail.com>
2023-09-28 00:37:35 -0300

    Login Background

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -7,18 +7,22 @@ import LoginPasswordReset from "./LoginPasswordReset";
 
 import { UserContext } from "../../context/UserContext";
 
+import styles from "./Login.module.css";
+
 function Login() {
   const { login } = React.useContext(UserContext);
 
   if (login) <Navigate to="/account" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="lost" element={<LogiPasswordLost />} />
-        <Route path="reset" element={<LoginPasswordReset />} />
-      </Routes>
+    <div className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="lost" element={<LogiPasswordLost />} />
+          <Route path="reset" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
     </div>
   );
 }
