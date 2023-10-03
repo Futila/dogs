3fa6dd92d714beef4572e16d9470b988db90bec1
Fernando Futila <fernando.futila@gmail.com>
2023-10-03 00:30:45 -0300

    added useFetch hook

--- src/components/Login/LoginCreate/index.jsx
+++ src/components/Login/LoginCreate/index.jsx
@@ -2,16 +2,20 @@ import React from "react";
 
 import { USER_POST } from "../../../services/api";
 import useForm from "../../../hooks/useForm";
+import useFetch from "../../../hooks/useFetch";
 import { UserContext } from "../../../context/UserContext";
 
 import Input from "../../Forms/Input";
 import Button from "../../Forms/Button";
+import Error from "../../../helpers/Error";
 
 function LoginCreate() {
-  const { userLogin } = React.useContext(UserContext);
   const username = useForm();
   const email = useForm("email");
   const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   const handleCreateAccount = async (event) => {
     event.preventDefault();
@@ -22,11 +26,8 @@ function LoginCreate() {
       password: password.value,
     });
 
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
-    const json = await response.json();
-
-    console.log(response);
   };
   return (
     <section className="animeLeft">
@@ -35,7 +36,12 @@ function LoginCreate() {
         <Input label="User" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Password" type="password" name="password" {...password} />
-        <Button>Create account</Button>
+        {loading ? (
+          <Button disabled>Creating account</Button>
+        ) : (
+          <Button>Create account</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import { useState, useCallback } from "react";
+
+const useFetch = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const request = useCallback(async (url, options) => {
+    let response;
+    let json;
+
+    try {
+      setError(null);
+      setLoading(true);
+
+      response = await fetch(url, options);
+      json = await response.json();
+
+      if (response.ok === false) throw new Error(json.message);
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return {
+    loading,
+    error,
+    request,
+  };
+};
+
+export default useFetch;
