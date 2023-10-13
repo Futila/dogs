d4c5d6caaacbc10ceca40a1361f2adfa7d4f75ee
Fernando Futila <fernando.futila@gmail.com>
2023-10-13 02:17:49 -0300

    added mobile menu

--- src/components/User/UserHeaderNav.jsx
+++ src/components/User/UserHeaderNav.jsx
@@ -1,7 +1,8 @@
-import { useContext, useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 
 import { UserContext } from "../../context/UserContext";
+import useMedia from "../../hooks/useMedia";
 
 import MyPhotos from "../../assets/feed.svg?react";
 import Stats from "../../assets/estatisticas.svg?react";
@@ -12,8 +13,16 @@ import styles from "./UserHeaderNav.module.css";
 
 const UserHeaderNav = () => {
   const { userLogout } = useContext(UserContext);
-  const [mobile, setMobile] = useState(null);
+
   const navigate = useNavigate();
+  const mobile = useMedia("(max-width: 40rem)");
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
   const handleUserLogout = () => {
     userLogout();
@@ -21,25 +30,41 @@ const UserHeaderNav = () => {
   };
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/account" end>
-        {" "}
-        <MyPhotos />
-        {mobile && "My Account"}
-      </NavLink>
-      <NavLink to="/account/stats">
-        <Stats />
-        {mobile && "Stats"}
-      </NavLink>
-      <NavLink to="/account/post">
-        <AddPhoto />
-        {mobile && "photo"}
-      </NavLink>
-      <button onClick={handleUserLogout}>
-        <Logout />
-        {mobile && "Logout"}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          onClick={() => setMobileMenu(!mobileMenu)}
+          className={`${styles.mobileButton}  ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          aria-label="Menu"
+        ></button>
+      )}
+
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav}  ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to="/account" end>
+          {" "}
+          <MyPhotos />
+          {mobile && "My Account"}
+        </NavLink>
+        <NavLink to="/account/stats">
+          <Stats />
+          {mobile && "Stats"}
+        </NavLink>
+        <NavLink to="/account/post">
+          <AddPhoto />
+          {mobile && "photo"}
+        </NavLink>
+        <button onClick={handleUserLogout}>
+          <Logout />
+          {mobile && "Logout"}
+        </button>
+      </nav>
+    </>
   );
 };
 
