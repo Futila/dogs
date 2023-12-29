d8b8d9c7a27348202d0e998e1907be9b52952c8b
Fernando Futila <fernando.futila@gmail.com>
2023-12-29 12:50:02 -0300

    Lazy and Suspense in UserStats

--- src/components/User/UserStats.jsx
+++ src/components/User/UserStats.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, Suspense, lazy } from "react";
 
 import Head from "../../helpers/Head";
 import useFetch from "../../hooks/useFetch";
@@ -8,7 +8,8 @@ import { STATS_GET } from "../../services/api";
 import Loading from "../../helpers/Loading";
 import Error from "../../helpers/Error";
 
-import UserStatsGraphs from "./UserStatsGraphs";
+// import UserStatsGraphs from "./UserStatsGraphs";
+const UserStatsGraphs = lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
   const { loading, error, data, request } = useFetch();
@@ -26,10 +27,12 @@ const UserStats = () => {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <div>
+      <Suspense fallback={<div></div>}>
         <Head title="Stats" />
+
+        {/* To load only when necessary */}
         <UserStatsGraphs data={data} />
-      </div>
+      </Suspense>
     );
   else return null;
 };
